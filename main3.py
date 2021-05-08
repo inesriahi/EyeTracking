34783bd76ee34479905a741f96d78a28deed66f3
NoussaOns <nosaadel99@gmail.com>
2021-05-08 16:40:44 +0400

    FIX: (-215)size.width>0 && size.height>0 in function imshow

--- main3.py
+++ main3.py
@@ -53,78 +53,79 @@ def process_eye(eye_frame, threshold):
 
 
 while True:
-    _, frame = cap.read()
+    ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = detector(gray) # array of all the faces
-    for face in faces:
-        x,y = face.left(), face.top()
-        x1, y1 = face.right(), face.bottom()
+    if ret:
+        faces = detector(gray) # array of all the faces
+        for face in faces:
+            x,y = face.left(), face.top()
+            x1, y1 = face.right(), face.bottom()
 
-        landmarks = predictor(gray, face)
+            landmarks = predictor(gray, face)
 
-        # Gaze Detection
-        left_eye_region = np.array([(landmarks.part(i).x, landmarks.part(i).y) for i in range(36,42)], np.int32)
+            # Gaze Detection
+            left_eye_region = np.array([(landmarks.part(i).x, landmarks.part(i).y) for i in range(36,42)], np.int32)
 
-        black_frame = np.zeros(gray.shape, np.uint8)
+            black_frame = np.zeros(gray.shape, np.uint8)
 
-        mask = np.full(gray.shape, 255, np.uint8)
-        # cv2.polylines(mask,[left_eye_region], True, 255)
-        cv2.fillPoly(mask, [left_eye_region], (0,0,0))
-        left_eye_masked = cv2.bitwise_not(black_frame, gray.copy(),mask=mask)
-        # cv2.imshow("", left_eye_masked)
+            mask = np.full(gray.shape, 255, np.uint8)
+            # cv2.polylines(mask,[left_eye_region], True, 255)
+            cv2.fillPoly(mask, [left_eye_region], (0,0,0))
+            left_eye_masked = cv2.bitwise_not(black_frame, gray.copy(),mask=mask)
+            # cv2.imshow("", left_eye_masked)
 
-        min_x = np.min(left_eye_region[:, 0]) - margin
-        max_x = np.max(left_eye_region[:, 0]) + margin
-        min_y = np.min(left_eye_region[:, 1]) - margin
-        max_y = np.max(left_eye_region[:, 1]) + margin
+            min_x = np.min(left_eye_region[:, 0]) - margin
+            max_x = np.max(left_eye_region[:, 0]) + margin
+            min_y = np.min(left_eye_region[:, 1]) - margin
+            max_y = np.max(left_eye_region[:, 1]) + margin
 
-        ## Eye
-        # Get the only the eye from the masked frame (with eye and white background)
-        small_eye = left_eye_masked[min_y:max_y, min_x:max_x]
-        cv2.imshow("Left Eye Masked Small Eye", small_eye)
+            ## Eye
+            # Get the only the eye from the masked frame (with eye and white background)
+            small_eye = left_eye_masked[min_y:max_y, min_x:max_x]
+            cv2.imshow("Left Eye Masked Small Eye", small_eye)
 
-        small_eye = process_eye(small_eye, best_threshold(small_eye))
+            small_eye = process_eye(small_eye, best_threshold(small_eye))
 
-        # Show the thresholded iris area in its original size
-        cv2.imshow("Small Eye resized", small_eye)
+            # Show the thresholded iris area in its original size
+            cv2.imshow("Small Eye resized", small_eye)
 
-        ## Frame reconstruction to replace the thresholded iris in its frame
-        white_frame = np.full(gray.shape, 255, np.uint8)
-        white_frame[min_y:max_y, min_x:max_x] = small_eye
-        cv2.imshow("Reconstructed Frame", white_frame)
+            ## Frame reconstruction to replace the thresholded iris in its frame
+            white_frame = np.full(gray.shape, 255, np.uint8)
+            white_frame[min_y:max_y, min_x:max_x] = small_eye
+            cv2.imshow("Reconstructed Frame", white_frame)
 
-        # Invert the image to make the iris white as it is the object
-        black_bg = cv2.bitwise_not(white_frame)
-        ## Finding Contours
-        contours, _ = cv2.findContours(black_bg.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        contours = sorted(contours, key=lambda c: cv2.contourArea(c), reverse=True)
+            # Invert the image to make the iris white as it is the object
+            black_bg = cv2.bitwise_not(white_frame)
+            ## Finding Contours
+            contours, _ = cv2.findContours(black_bg.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contours = sorted(contours, key=lambda c: cv2.contourArea(c), reverse=True)
 
-        ## for each contour
-        for cnt in contours:
-            # compute the center of the contour
-            M = cv2.moments(cnt)
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
+            ## for each contour
+            for cnt in contours:
+                # compute the center of the contour
+                M = cv2.moments(cnt)
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
 
-            #draw the contour and center of the shape on the image
-            cv2.drawContours(black_bg, [cnt], -1, (0,255,0), 2)
-            cv2.circle(frame, (cX, cY), 2, (0,255,0),-1)
-            cv2.putText(frame, f'Coordinates of pupil: ({cX},{cY})', (30,50), 0,0.5,(0,0,0))
-            break
+                #draw the contour and center of the shape on the image
+                cv2.drawContours(black_bg, [cnt], -1, (0,255,0), 2)
+                cv2.circle(frame, (cX, cY), 2, (0,255,0),-1)
+                cv2.putText(frame, f'Coordinates of pupil: ({cX},{cY})', (30,50), 0,0.5,(0,0,0))
+                break
 
-        ### Detect Blinking
-        left_eye_ratio = get_blinking_ratio(list(range(36,42)), landmarks)
-        right_eye_ratio = get_blinking_ratio(list(range(42,48)), landmarks)
-        blinking_ratio = (left_eye_ratio + right_eye_ratio) / 2
+            ### Detect Blinking
+            left_eye_ratio = get_blinking_ratio(list(range(36,42)), landmarks)
+            right_eye_ratio = get_blinking_ratio(list(range(42,48)), landmarks)
+            blinking_ratio = (left_eye_ratio + right_eye_ratio) / 2
 
-        if blinking_ratio > 5.5:
-            cv2.putText(frame, "Blinking", (30,100), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255,0,0))
+            if blinking_ratio > 5.5:
+                cv2.putText(frame, "Blinking", (30,100), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255,0,0))
 
 
-        cv2.imshow('Black Bg', black_bg)
+            cv2.imshow('Black Bg', black_bg)
 
-    cv2.imshow("Frame", frame)
+        cv2.imshow("Frame", frame)
 
     key = cv2.waitKey(1)
     if key == 27:
