a308b62499444237e4c798930719bfb5ad553370
NoussaOns <nosaadel99@gmail.com>
2021-05-10 21:56:47 +0400

    Changes by Aafaq in helpers.py (Finalizing Comments)

--- helpers.py
+++ helpers.py
@@ -102,10 +102,12 @@ def best_threshold(eye_frame):
     # trials array to choose the best from them
     trials = {}
 
+    # loop through threshold values and apply it to eye frame
     for threshold in range(5,100,5):
         iris_frame = process_eye(eye_frame, threshold)
         trials[threshold] = iris_size(iris_frame)
 
+    # get the best threshold from all trials
     best_thresh, best_iris_size = min(trials.items(), key=(lambda p: abs(p[1]-average_iris_size)))
 
     return best_thresh
@@ -118,6 +120,8 @@ def get_eye_center(landmarks):
     :return: the x and y coordinates of the eye center
     '''
     ## TODO: Find better get center logic
+
+    # get eye center location using estimations from dlib points
     mid_eye_x = (landmarks.part(37).x + landmarks.part(38).x) // 2
 
     # mid_eye_x = (landmarks.part(36).x + landmarks.part(39).x)//2
@@ -141,7 +145,9 @@ def eye_direction_distance(landmarks, pupil):
     :param pupil: The coordinates of the Pupil
     :return: angle in degrees and distance
     '''
+    # get eye center
     eyeCenter = get_eye_center(landmarks)
+    # compute gaze direction by calculating the angle and the distance between the pupil and eye center
     angle = math.degrees(math.atan2(eyeCenter[1]-pupil[1], eyeCenter[0]-pupil[0]))
     dist = distance(pupil,eyeCenter)
 
@@ -155,8 +161,11 @@ def get_direction_description(detected_angle, detected_distance):
     :param detected_distance: distance between eye center and pupil
     :return: text description for the position of the pupil
     '''
+    # if the distance between pupil and eye center is less than a specific value,
+    # the gaze is estimated to be at the center
     if detected_distance < 3.5:
         return "center"
+    # dictionary that stores the directions of pupil with their angle from eye center
     directions = {
         "top" : 90,
         "bottom" : -90,
@@ -167,6 +176,8 @@ def get_direction_description(detected_angle, detected_distance):
         "top_left" :  45,
         "bottom_left" : -45
     }
-
+    # compute the best estimation of pupil direction, by finding the minimum difference
+    # between the detected angle and the angle of each direction (from the directions dictionary)
     dir_name, nearest_anchor = min(directions.items(), key=(lambda a: abs(detected_angle - a[1])))
+
     return dir_name
