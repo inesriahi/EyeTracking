efcecfaa14a4a0d82322474dbf60d878beb6732e
NoussaOns <nosaadel99@gmail.com>
2021-05-11 16:23:58 +0400

    Testing Accuracy on BioID dataset

--- try1.py
+++ try1.py
@@ -1,198 +1,141 @@
-from __future__ import division
-import cv2
+import os
+import matplotlib.pyplot as plt
 import numpy as np
-import math
-# from scipy.misc import toimage
-import time
-from queue import *
+import seaborn as sns
+from helpers import *
 
-# test a possible center (x,y) are coors of possible
-# center and gx and gy are the x and y components
-# of the gradient of some other point x.
-K_WEIGHT_DIVISOR = 1.0
-K_FAST_WIDTH = 50
-K_GRADIENT_THRESHOLD = 50.0
-K_WEIGHT_BLUR_SIZE = 5
-K_THRESHOLD_VALUE = 0.60
-K_ENABLE_WEIGHT = True
-K_POST_PROCESSING = True
+dir = 'test_imgs'
+# initialize dlib face detector
+detector = dlib.get_frontal_face_detector()
+# initialize facial landmark predictor
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-# Helpers
-def unscale_point(p, orig):
-    px, py = p
-    height, width = orig.shape
-    ratio = K_FAST_WIDTH/width
-    x = int(round(px / ratio))
-    y = int(round(py / ratio))
-    return (x,y)
+all_files = sorted(os.listdir(dir))
 
+count = 20
+errors = []
 
-def scale_to_fast_size(src):
-    rows, cols = src.shape
-    return cv2.resize(src, (K_FAST_WIDTH, int((K_FAST_WIDTH / cols) * rows)))
+for i in range(len(all_files)):
+    file_dir = ''.join([dir, '/', all_files[i]])
+    if file_dir.endswith('.eye'):
+        file_dir_eye = ''.join([dir, '/', all_files[i]])
+        file_dir_pgm = ''.join([dir, '/', all_files[i + 1]])
+        with open(file_dir_eye, 'r') as coor_file:
+            coor_file.readline()  # skip header
+            x, y = map(int, coor_file.readline().split()[2:])
+            real_pupil = (x, y)
+        frame = cv2.imread(file_dir_pgm)
 
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # rotate the frame horizontally
+        # frame = cv2.flip(frame, 1)
+        # gray = cv2.flip(gray, 1)
 
-def test_possible_centers_formula(x, y, weight, gx, gy, arr):
-    rows, cols = np.shape(arr)
-    for cy in range(rows):
-        for cx in range(cols):
-            if x == cx and y == cy:
-                continue
-            dx = x - cx
-            dy = y - cy
+        # array of all the faces
+        faces = detector(gray)
+        # loop over the faces frames
+        found = True
+        for face in faces:
+            # get face frame corners
+            x, y = face.left(), face.top()
+            x1, y1 = face.right(), face.bottom()
+            # get facial landmarks for the face
+            landmarks = predictor(gray, face)
 
-            magnitude = math.sqrt((dx * dx) + (dy * dy))
-            dx = dx / magnitude
-            dy = dy / magnitude
-            dot_product = dx * gx + dy * gy
-            dot_product = max(0.0, dot_product)
-            if K_ENABLE_WEIGHT == True:
-                arr[cy][cx] += dot_product * dot_product * (weight[cy][cx]/K_WEIGHT_DIVISOR)
-            else:
-                arr[cy][cx] += dot_product * dot_product
-    return arr
+            # Gaze Detection
 
+            # get left eye points
+            left_eye_region = np.array([(landmarks.part(i).x, landmarks.part(i).y) for i in range(36, 42)],
+                                       np.int32)
+            # create a black frame with frame size
+            black_frame = np.zeros(gray.shape, np.uint8)
+            # create a mask to extract the left eye
+            mask = np.full(gray.shape, 255, np.uint8)
+            # cv2.polylines(mask,[left_eye_region], True, 255)
+            # draw mask of left eye area
+            cv2.fillPoly(mask, [left_eye_region], (0, 0, 0))
+            # get the cropped area of the mask
+            left_eye_masked = cv2.bitwise_not(black_frame, gray.copy(), mask=mask)
+            # cv2.imshow("", left_eye_masked)
 
-def matrix_magnitude(mat_x, mat_y):
-    rows, cols = np.shape(mat_x)
-    res_arr = np.zeros((rows, cols))
-    for y in range(rows):
-        for x in range(cols):
-            gX = mat_x[y][x]
-            gY = mat_y[y][x]
-            magnitude = math.sqrt((gX * gX) + (gY * gY))
-            res_arr[y][x] = magnitude
-    return res_arr
+            # get eye detection rectangle coordinates
+            min_x = np.min(left_eye_region[:, 0]) - margin
+            max_x = np.max(left_eye_region[:, 0]) + margin
+            min_y = np.min(left_eye_region[:, 1]) - margin
+            max_y = np.max(left_eye_region[:, 1]) + margin
 
+            ## Eye
+            # Get the only the eye from the masked frame (with eye and white background)
+            small_eye = left_eye_masked[min_y:max_y, min_x:max_x]
+            # cv2.imshow("Left Eye Masked Small Eye", small_eye)
+            # Process the eye with the best threshold
+            small_eye = process_eye(small_eye, best_threshold(small_eye))
 
-def compute_dynamic_threshold(mags_mat, std_dev_factor):
-    mean_magn_grad, std_magn_grad = cv2.meanStdDev(mags_mat)
-    rows, cols = np.shape(mags_mat)
-    stddev = std_magn_grad[0] / math.sqrt(rows * cols)
-    return std_dev_factor * stddev + mean_magn_grad[0]
+            # Show the thresholded iris area in its original size
+            # cv2.imshow("Small Eye resized", small_eye)
 
+            ## Frame reconstruction to replace the thresholded iris in its frame
+            white_frame = np.full(gray.shape, 255, np.uint8)
+            white_frame[min_y:max_y, min_x:max_x] = small_eye
+            # cv2.imshow("Reconstructed Frame", white_frame)
 
-def flood_should_push_point(dir, mat):
-    px, py = dir
-    rows, cols = np.shape(mat)
-    if px >= 0 and px < cols and py >= 0 and py < rows:
-        return True
-    else:
-        return False
+            # Invert the image to make the iris white as it is the object
+            black_bg = cv2.bitwise_not(white_frame)
+            ## Finding Contours
+            contours, _ = cv2.findContours(black_bg.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contours = sorted(contours, key=lambda c: cv2.contourArea(c), reverse=True)
 
+            # skip closed eyes images
+            if len(contours) == 0:
+                found = False
+                break
+            ## for each contour
+            # compute the center of the contour
+            for cnt in contours:
+                # Get the image moments to get the center of the image
+                M = cv2.moments(cnt)
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
 
-def flood_kill_edges(mat):
-    rows, cols = np.shape(mat)
-    cv2.rectangle(mat, (0,0), (cols, rows), 255)
-    mask = np.ones((rows, cols), dtype=np.uint8)
-    mask = mask * 255
-    to_do = Queue()
-    to_do.put((0,0))
-    while to_do.qsize() > 0:
-        px,py = to_do.get()
-        if mat[py][px] == 0:
+                # Center of the iris is the pupil
+                detected_pupil = (cX, cY)
+                # draw the contour and center of the shape on the image
+
+                # cv2.drawContours(black_bg, [cnt], -1, (0, 255, 0), 2)
+                # cv2.circle(frame, (cX, cY), 2, (0, 255, 0), -1)
+                # cv2.putText(frame, f'Coordinates of pupil: ({cX},{cY})', (30, 50), 0, 0.5, (0, 0, 0))
+                break
+
+        if not found:
             continue
-        right = (px + 1, py)
-        if flood_should_push_point(right, mat):
-            to_do.put(right)
-        left = (px - 1, py)
-        if flood_should_push_point(left, mat):
-            to_do.put(left)
-        down = (px, py + 1)
-        if flood_should_push_point(down, mat):
-            to_do.put(down)
-        top = (px, py - 1)
-        if flood_should_push_point(top, mat):
-            to_do.put(top)
-        mat[py][px] = 0.0
-        mask[py][px] = 0
-    return mask
+
+        # cv2.circle(frame, real_pupil, 2, (0, 0, 255), -1)
+
+        # cv2.circle(frame, (x_left, y_left),2, (0,255,0),1)
+        # frame = cv2.flip(frame, 1)
+        # frame = cv2.resize(frame, None, fx=3,fy=3)
+
+        # cv2.imshow(all_files[i + 1], frame)
+
+        # count -= 1
+        # if count == 0:
+        #     break
+        errors.append(distance(detected_pupil, real_pupil))
+
+errors = np.array(errors)
+print('Number of images:',errors.shape)
+print('Max error:', errors.max())
+print('Min error:', errors.min())
+print('Quantiles:', np.quantile(errors, [0.25,0.5,.75]))
+hist = sns.histplot(errors, color='salmon')
+hist.set(xlim=(0,20))
+plt.xlabel('Error (Euclidean distance between real and predicted pupil)')
+# plt.xticks(range(0,5))
+# plt.xlim(0,6)
+plt.show()
+# Calculating the error
+# print(all_files[i + 1] + ': Detected Pupil:', detected_pupil, 'Real Pupil:', real_pupil)
 
 
-def compute_mat_x_gradient(mat):
-    rows, cols = mat.shape
-    out = np.zeros((rows, cols), dtype='float64')
-    mat = mat.astype(float)
-    for y in range(rows):
-        out[y][0] = mat[y][1] - mat[y][1]
-        for x in range(cols - 1):
-            out[y][x] = (mat[y][x+1] - mat[y][x-1])/2.0
-        out[y][cols - 1] = (mat[y][cols - 1] - mat[y][cols - 2])
-    return out
-
-
-
-def find_eye_center(img):
-    # get row and column lengths
-    rows, cols = np.asarray(img).shape
-
-    # scale down eye image to manageable size
-    resized = scale_to_fast_size(img)
-    resized_arr = np.asarray(resized)
-    res_rows, res_cols = np.shape(resized_arr)
-
-    # compute gradients for x and y components of each point
-    grad_arr_x = compute_mat_x_gradient(resized_arr)
-    grad_arr_y = np.transpose(compute_mat_x_gradient(np.transpose(resized_arr)))
-
-    # create a matrix composed of the magnitudes of the x and y gradients
-    mags_mat = matrix_magnitude(grad_arr_x, grad_arr_y)
-
-    # find a threshold value to get rid gradients that are below gradient threshold
-    gradient_threshold = compute_dynamic_threshold(mags_mat, K_GRADIENT_THRESHOLD)
-    # and now set those gradients to 0 if < gradient threshold and scale down other
-    # gradients
-
-    for y in range(res_rows):
-        for x in range(res_cols):
-            gX = grad_arr_x[y][x]
-            gY = grad_arr_y[y][x]
-            mag = mags_mat[y][x]
-            if mag > gradient_threshold:
-                grad_arr_x[y][x] = gX/mag
-                grad_arr_y[y][x] = gY/mag
-            else:
-                grad_arr_x[y][x] = 0.0
-                grad_arr_y[y][x] = 0.0
-
-    # create a weighted image that has a gausian blur
-    weight = cv2.GaussianBlur(resized, (K_WEIGHT_BLUR_SIZE, K_WEIGHT_BLUR_SIZE), 0, 0)
-    weight_arr = np.asarray(weight)
-    weight_rows, weight_cols = np.shape(weight_arr)
-    # invert the weight matrix
-    for y in range(weight_rows):
-        for x in range(weight_cols):
-            weight_arr[y][x] = 255-weight_arr[y][x]
-
-    # create a matrix to store the results from test_possible_centers_formula
-    out_sum = np.zeros((res_rows, res_cols))
-    out_sum_rows, out_sum_cols = np.shape(out_sum)
-
-    # call test_possible_centers for each point
-    for y in range(weight_rows):
-        for x in range(weight_cols):
-            gX = grad_arr_x[y][x]
-            gY = grad_arr_y[y][x]
-            if gX == 0.0 and gY == 0.0:
-                continue
-            test_possible_centers_formula(x, y, weight_arr, gX, gY, out_sum)
-    # average all values in out_sum and convert to float32. assign to 'out' matrix
-    num_gradients = weight_rows * weight_cols
-    out = out_sum.astype(np.float32)*(1/num_gradients)
-    _, max_val, _, max_p = cv2.minMaxLoc(out)
-    print (max_p)
-    if K_POST_PROCESSING == True:
-        flood_thresh = max_val * K_THRESHOLD_VALUE
-        retval, flood_clone = cv2.threshold(out, flood_thresh, 0.0, cv2.THRESH_TOZERO)
-        mask = flood_kill_edges(flood_clone)
-        _, max_val, _, max_p = cv2.minMaxLoc(out, mask)
-        print (max_p)
-    x, y = unscale_point(max_p, img)
-    return x,y
-
-
-img = cv2.imread('try1.png',0)
-center = find_eye_center(img)
-cv2.circle(img, center, 5, (255,0,0))
-cv2.imshow('final', img)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
